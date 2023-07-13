14b2da74781b7695719eb940e03205872d9fd548
asano <asanokeenta@me.com>
2023-07-13 17:05:16 +0900

    プロフィール編集機能追加

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
     config.resolve.fallback = { fs: false };
     return config;
   },
+  reactStrictMode: true,
+  images: {
+    domains: [process.env.NEXT_PUBLIC_SUPABASE_URL],
+  },
 };
 
 module.exports = nextConfig;
